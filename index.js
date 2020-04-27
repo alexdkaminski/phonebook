34bc202d90f56c61339539c1bf00a6b684c6c7ac
Alex Kaminski <alexdkaminski@gmail.com>
2020-04-27 10:11:13 +1000

    Revert "Revert "Add exercise 3.1-3.8""
    This reverts commit b3b96a0e97190242c2bedb352d796ef747359b4e.

---
+++ index.js
@@ -0,0 +1,109 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req,res), 'ms',
+    tokens.body(req,res)
+  ].join(' ')
+}))
+
+app.use(express.json())
+
+let persons =
+  [
+    {
+      "name": "Michael Rooney",
+      "number": "12345678",
+      "id": 1
+    },
+    {
+      "name": "Alex Kaminski",
+      "number": "56781234",
+      "id": 2
+    },
+    {
+      "name": "Katie Dangerfield",
+      "number": "98761234",
+      "id": 3
+    }
+  ]
+
+app.get('/api/persons', (req,res) => {
+  res.json(persons)
+})
+
+app.get('/api/info', (req,res) => {
+  const personsCount = persons.length
+  const timeStamp = new Date()
+  const content = `
+  <p>Phonebook has info for ${personsCount} persons</p>
+  <p>${timeStamp}</p>
+  `;
+  res.send(content)
+})
+
+app.get('/api/persons/:id', (req,res) => {
+  const id = Number(req.params.id)
+  console.log(id);
+
+  const person = persons.find(person => person.id === id)
+  console.log(person)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req,res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req,res) => {
+  const body = req.body
+  const createId = () => {
+      return (Math.floor(Math.random()*10000))
+  }
+
+  let nameExists = persons.find(person => person.name === body.name)
+
+  if(!body.name) {
+    return res.status(400).json({
+      error: 'name missing'
+    })
+  } else if (!body.number) {
+    return res.status(400).json({
+      error: 'number missing'
+    })
+  } else if (nameExists) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: createId()
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
+})
+
+const port = 3001
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
